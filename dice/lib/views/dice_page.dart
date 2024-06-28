82f83f5b4cd2f7e3f32cb0fe04dfe3bfaa61a788
Win Htun Oo <winhtunoo@onenex.co>
2024-06-28 22:18:17 +0700

    change stateless widget to stateful widget

--- dice/lib/views/dice_page.dart
+++ dice/lib/views/dice_page.dart
@@ -1,19 +1,30 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   const DicePage({super.key});
 
   @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var dices = [1,2,3,4,5,6];
+  int leftDiceNumber = 1;
+  @override
   Widget build(BuildContext context) {
+    final random = Random();
     return Center(
       child: Row(
         children: [
           Expanded(
             child: TextButton(
               onPressed: () {
-                print('left button');
+                setState(() {
+                  leftDiceNumber = dices[random.nextInt(dices.length)];
+                });  
               },
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
           Expanded(
